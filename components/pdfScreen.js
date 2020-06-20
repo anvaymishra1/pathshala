477a8195147f82933d98a3c0d42e1d66a0c8bb84
Aditya Prakash <p.aditya.198@gmail.com>
2020-06-20 10:46:16 +0530

    backend integration

--- components/pdfScreen.js
+++ components/pdfScreen.js
@@ -1,14 +1,27 @@
+/* eslint-disable no-console */
 import React from 'react'
 import styles from './pdfScreen.module.scss'
 import ParticipantCard from './participantCard'
 
+import back from '../util/back'
+
 // @todo Add Mouse Pointer
 
 function PDFScreen() {
+  const uploadHandler = async () => {
+    // eslint-disable-next-line no-return-await
+    return await back
+      .post('/upload')
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => console.log(err))
+  }
   return (
     <>
       <div className={styles.left} id="screen">
         <h1>PDF Screen</h1>
+        <button onClick={uploadHandler}>+</button>
       </div>
       <div className={styles.right}>
         <ParticipantCard props="Participant List" />
